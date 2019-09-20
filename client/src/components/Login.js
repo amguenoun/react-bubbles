f7281f1d21b71f713ec436f5ac5e575048ac39f7
Ahmad Guenoun <amguenoun@gmail.com>
2019-09-20 11:37:30 -0400

    got rid of unneeded comment

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -26,9 +26,6 @@ const Login = (props) => {
       .catch(err => console.log('Error: Login.js: Post: ', err));
   }
 
-
-  // make a post request to retrieve a token from the api
-  // when you have handled the token, navigate to the BubblePage route
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
