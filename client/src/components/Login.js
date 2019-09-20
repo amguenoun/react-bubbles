f04a157f93fdbcf79b81909eaaba52be4c5aa0a4
Ahmad Guenoun <amguenoun@gmail.com>
2019-09-20 11:25:54 -0400

    created login form and changed server user and pass to admin

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from 'axios';
 
-const Login = () => {
+const initialUser = {
+  username: '',
+  password: ''
+}
+
+const Login = (props) => {
+  const [user, setUser] = useState(initialUser);
+
+  const handleUserChange = e => {
+    setUser({
+      ...user,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  const handleUserSubmit = e => {
+    e.preventDefault();
+    axios.post(`http://localhost:5000/api/login`, user)
+      .then(res => {
+        localStorage.setItem('token', res.data.payload);
+        props.history.push('/bubblepage');
+      })
+      .catch(err => console.log('Error: Login.js: Post: ', err));
+  }
+
+
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+      <form onSubmit={handleUserSubmit}>
+        <input name='username' placeholder='Username' value={user.username} onChange={handleUserChange} />
+        <input type='password' name='password' placeholder='Password' value={user.password} onChange={handleUserChange} />
+        <button>Login</button>
+      </form>
     </>
   );
 };
